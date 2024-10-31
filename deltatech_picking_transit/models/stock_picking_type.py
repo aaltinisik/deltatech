13aeb5134ff71fc8c3e00dd8382502fdb869c88b
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-10-31 10:29:42 +0200

    [16.0][UPD] deltatech_picking_transit

--- deltatech_picking_transit/models/stock_picking_type.py
+++ deltatech_picking_transit/models/stock_picking_type.py
@@ -6,4 +6,6 @@ from odoo import fields, models
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
-    next_operation_id = fields.Many2one("stock.picking.type", string="Next Operation")
+    two_step_transfer_use = fields.Selection(
+        [("reception", "Reception"), ("delivery", "Delivery")], string="Two Step Transfer Use"
+    )
