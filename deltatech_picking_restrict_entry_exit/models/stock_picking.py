24fb6fc95b39c26e46b37a42b29aafb5ef8dbeb8
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-12-05 13:36:31 +0200

    [17.0][UPD] picking_restrict_etry_exit picking_transit

--- deltatech_picking_restrict_entry_exit/models/stock_picking.py
+++ deltatech_picking_restrict_entry_exit/models/stock_picking.py
@@ -10,7 +10,7 @@ class StockPicking(models.Model):
         for vals in vals_list:
             if not self.env.user.has_group("deltatech_picking_restrict_entry_exit.group_picking_restrict_entry_exit"):
                 picking_type = self.env["stock.picking.type"].browse(vals.get("picking_type_id"))
-                if picking_type.code =="outgoing":
+                if picking_type.code == "outgoing":
                     if not vals.get("sale_id"):
                         raise UserError(_("You cannot create an outgoing picking without a source sale order."))
                 elif picking_type.code == "incoming":
@@ -22,13 +22,30 @@ class StockPicking(models.Model):
     # self.env.user.has_group("deltatech_picking_restrict_entry_exit.group_picking_restrict_entry_exit")
     def button_validate(self):
         for picking in self:
-            if picking.picking_type_id.code in ["incoming", "outgoing"]:
-                for move in picking.move_ids:
-                    if move.quantity > move.product_uom_qty:
+            picking_type = picking.picking_type_id
+            for move in picking.move_ids:
+                if picking_type.code == "outgoing":
+                    if not move.sale_line_id:
                         raise UserError(
                             _(
-                                "You cannot validate the picking because the quantity done is greater than the quantity ordered for the product %s."
+                                "You cannot validate the picking because the product %s is not linked to a sale order line."
                             )
                             % move.product_id.display_name
                         )
+                elif picking_type.code == "incoming":
+                    if not move.purchase_line_id:
+                        raise UserError(
+                            _(
+                                "You cannot validate the picking because the product %s is not linked to a purchase order line."
+                            )
+                            % move.product_id.display_name
+                        )
+                if move.quantity > move.product_uom_qty:
+                    raise UserError(
+                        _(
+                            "You cannot validate the picking because the quantity done is greater than the quantity ordered for the product %s."
+                        )
+                        % move.product_id.display_name
+                    )
+
         return super().button_validate()
