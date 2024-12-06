0e4b450150a7b7684bc167451bf8453ee2664544
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-12-06 14:28:47 +0200

    [17.0][UPD] deltatech_record_type

--- deltatech_record_type/__manifest__.py
+++ deltatech_record_type/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Terrabit - Record Type",
     "summary": "Manage multiple record types",
-    "version": "17.0.1.0.4",
+    "version": "17.0.1.0.5",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules/Other",
