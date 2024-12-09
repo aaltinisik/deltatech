7caaed3cc9af48f3f127fa15e12748541f944dcb
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-12-09 10:04:55 +0200

    [17.0][UPD] deltatech_picking_Restict_entry_exit

--- deltatech_picking_restrict_entry_exit/models/stock_picking.py
+++ deltatech_picking_restrict_entry_exit/models/stock_picking.py
@@ -10,12 +10,13 @@ class StockPicking(models.Model):
         for vals in vals_list:
             if not self.env.user.has_group("deltatech_picking_restrict_entry_exit.group_picking_restrict_entry_exit"):
                 picking_type = self.env["stock.picking.type"].browse(vals.get("picking_type_id"))
-                if picking_type.code == "outgoing":
-                    if not vals.get("sale_id"):
-                        raise UserError(_("You cannot create an outgoing picking without a source sale order."))
-                elif picking_type.code == "incoming":
-                    if not vals.get("purchase_id"):
-                        raise UserError(_("You cannot create an incoming picking without a source purchase order."))
+                if not vals.get("return_id", False) and not vals.get("backorder_id", False):
+                    if picking_type.code == "outgoing":
+                        if not vals.get("sale_id"):
+                            raise UserError(_("You cannot create an outgoing picking without a source sale order."))
+                    elif picking_type.code == "incoming":
+                        if not vals.get("purchase_id"):
+                            raise UserError(_("You cannot create an incoming picking without a source purchase order."))
 
         return super().create(vals_list)
 
@@ -49,3 +50,40 @@ class StockPicking(models.Model):
                     )
 
         return super().button_validate()
+
+    def write(self, vals):
+        if 'move_ids_without_package' in vals:
+            for move_data in vals['move_ids_without_package']:
+                if len(move_data) > 2 and 'quantity' in move_data[2]:
+                    move_id = move_data[1]
+                    quantity = move_data[2]['quantity']
+                    move = self.env['stock.move'].browse(move_id)
+                    if quantity > move.product_uom_qty:
+                        raise UserError(
+                            _(
+                                "You cannot save the picking because the quantity done is greater than the quantity ordered for the product %s."
+                            )
+                            % move.product_id.display_name
+                        )
+
+        for picking in self:
+            picking_type = picking.picking_type_id
+            for move in picking.move_ids:
+                if picking_type.code == "outgoing":
+                    if not move.sale_line_id:
+                        raise UserError(
+                            _(
+                                "You cannot save the picking because the product %s is not linked to a sale order line."
+                            )
+                            % move.product_id.display_name
+                        )
+                elif picking_type.code == "incoming":
+                    if not move.purchase_line_id:
+                        raise UserError(
+                            _(
+                                "You cannot save the picking because the product %s is not linked to a purchase order line."
+                            )
+                            % move.product_id.display_name
+                        )
+
+        return super().write(vals)
